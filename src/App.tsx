8b76a621f6c4dbc57213dc0df03fe7a672067a98
carstrb <cstraub@dal.ca>
2025-11-23 14:02:31 -0330

    Enforce tab indentation using ESlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <div className='container'>
-          <div className='header'>
+	return (
+		<>
+			<div className='container'>
+				<div className='header'>
             Welcome to Pinball Wizard!
-          </div>
-      </div>
-    </>
-  )
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default App
